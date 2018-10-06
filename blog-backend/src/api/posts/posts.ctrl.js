9b9cf72d2116284889b4d036d03ad8e05269d718
Song Jewoo <iamsjy0808@gmail.com>
2018-10-07 00:40:20 +0900

    [추가] api tag분류 기능

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -64,6 +64,8 @@ exports.list = async (ctx) => {
   // page가 주어지지 않았다면 1로 간주
   // query는 문자열 형태로 받아오므로 숫자로 변환
   const page = parseInt(ctx.query.page || 1, 10);
+  const { tag } = ctx.query;
+  const query = tag ? { tags: tag } : {};
 
   // 잘못된 페이지가 주어졌다면 오류
   if (page < 1) {
@@ -72,13 +74,13 @@ exports.list = async (ctx) => {
   }
 
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.count(query).exec();
     const limitBodyLength = post => ({
       ...post,
       body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
